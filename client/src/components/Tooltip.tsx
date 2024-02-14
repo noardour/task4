abb2285db8a52ef25a981aca1789fc2b99852f23
Zadorin Artyom <noardour@gmail.com>
2024-02-15 04:39:28 +0500

    craete Tooltip component

---
+++ client/src/components/Tooltip.tsx
@@ -0,0 +1,31 @@
+import { FC } from "react";
+import Container from "./Container";
+import Button from "./UI/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLock,
+  faLockOpen,
+  faTrashCan,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Tooltip: FC = () => (
+  <div>
+    <Container>
+      <div className="bg-[#232323] p-4 rounded-lg">
+        <div className="flex gap-3">
+          <Button>
+            <FontAwesomeIcon icon={faLock} /> Block
+          </Button>
+          <Button>
+            <FontAwesomeIcon icon={faLockOpen} />
+          </Button>
+          <Button color="red">
+            <FontAwesomeIcon icon={faTrashCan} />
+          </Button>
+        </div>
+      </div>
+    </Container>
+  </div>
+);
+
+export default Tooltip;
