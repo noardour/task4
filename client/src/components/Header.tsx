9d2b3f050df6cb263e75699c96b712bb482b5c8c
Zadorin Artyom <noardour@gmail.com>
2024-02-15 04:38:16 +0500

    create Header component

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+import { Link } from "react-router-dom";
+import Container from "./Container";
+
+const Header: FC = () => (
+  <header className="bg-[#232323] p-4 mb-10">
+    <Container>
+      <div className="flex justify-end gap-3">
+        <Link className="font-semibold hover:text-[#60dbfa] text-xl" to="/">
+          home
+        </Link>
+        <Link
+          className="font-semibold hover:text-[#60dbfa] text-xl"
+          to="/login"
+        >
+          login
+        </Link>
+        <Link
+          className="font-semibold hover:text-[#60dbfa] text-xl"
+          to="/registration"
+        >
+          register
+        </Link>
+      </div>
+    </Container>
+  </header>
+);
+
+export default Header;
