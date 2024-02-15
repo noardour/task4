d3f2a23055dc8b92542981c061213b6c220731c3
Zadorin Artyom <noardour@gmail.com>
2024-02-15 15:59:03 +0500

    fix

--- client/src/routes/Root/index.tsx
+++ client/src/routes/Root/index.tsx
@@ -2,38 +2,13 @@ import { FC } from "react";
 import Container from "../../components/Container";
 import Tooltip from "../../components/Tooltip";
 import UsersTable from "../../components/UsersTable";
-import { IUser } from "../../types/IUser";
-
-const users: IUser[] = [
-  {
-    id: 1,
-    name: "FirstName SecondName",
-    eMail: "testtest@email.com",
-    lastLogin: "2022.01.01",
-    status: "active",
-  },
-  {
-    id: 2,
-    name: "FirstName SecondName",
-    eMail: "testtest@email.com",
-    lastLogin: "2022.01.01",
-    status: "active",
-  },
-  {
-    id: 3,
-    name: "FirstName SecondName",
-    eMail: "testtest@email.com",
-    lastLogin: "2022.01.01",
-    status: "active",
-  },
-];
 
 const Root: FC = () => (
   <div>
     <Container>
       <Tooltip />
 
-      <UsersTable users={users} />
+      <UsersTable />
     </Container>
   </div>
 );
