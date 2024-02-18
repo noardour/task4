32b520d8fbf8c894952ad3df79c89538ce7ec2d5
Zadorin Artyom <noardour@gmail.com>
2024-02-18 07:10:13 +0500

    setup prisma add routes

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,18 @@
 import express, { Express, Request, Response } from "express";
 import path from "path";
+import router from "./router";
 
 const app: Express = express();
 
-const buildDir = path.join(__dirname, "..", "client", "dist");
+app.use(express.json());
 
-app.use(express.static(buildDir));
+const clientDir = path.join(__dirname, "..", "client", "dist");
 
+app.use("/", router);
+
+app.use(express.static(clientDir));
 app.get("*", (req: Request, res: Response) => {
-  res.sendFile(path.join(buildDir, "index.html"));
+  res.sendFile(path.join(clientDir, "index.html"));
 });
 
 const port = process.env.PORT || 8080;
---
+++ server/router/apiRouter.ts
@@ -0,0 +1,98 @@
+import { Request, Response, Router } from "express";
+import { PrismaClient } from "@prisma/client";
+import Joi from "joi";
+const prisma = new PrismaClient();
+
+const apiRouter = Router();
+
+apiRouter.get("/users", async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+apiRouter.post("/users/create", async (req: Request, res: Response) => {
+  const schema = Joi.object({
+    email: Joi.string().email().max(255).required(),
+    name: Joi.string().min(1).max(255).required(),
+    password: Joi.string().min(1).max(255).required(),
+    repeat_password: Joi.ref("password"),
+  });
+  const validated = schema.validate(req.body);
+  const existUser = await prisma.user.findUnique({ where: { email: req.body?.email } });
+  let error: string | undefined;
+
+  if (existUser) {
+    error = "This email already registered.";
+  }
+  error = error || validated.error?.details[0].message;
+
+  if (error) {
+    res.status(422);
+    return res.send(error);
+  }
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: req.body.email,
+        name: req.body.name,
+        password: req.body.password,
+      },
+    });
+
+    res.json(user);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+apiRouter.patch("/users/block", async (req: Request, res: Response) => {
+  const users = await prisma.user.updateMany({
+    where: {
+      id: {
+        in: req.body.ids,
+      },
+    },
+    data: {
+      status: "BLOCKED",
+    },
+  });
+
+  res.json(users);
+});
+
+apiRouter.patch("/users/unblock", async (req: Request, res: Response) => {
+  const users = await prisma.user.updateMany({
+    where: {
+      id: {
+        in: req.body.ids,
+      },
+    },
+    data: {
+      status: "ACTIVE",
+    },
+  });
+
+  res.json(users);
+});
+
+apiRouter.delete("/users/delete/", async (req: Request, res: Response) => {
+  const ids = req.query.ids;
+
+  if (ids && typeof ids === "string") {
+    const users = await prisma.user.deleteMany({
+      where: {
+        id: {
+          in: JSON.parse(ids),
+        },
+      },
+    });
+
+    return res.json(req.query.ids);
+  }
+
+  res.status(422);
+  res.send("no ids passed");
+});
+
+export default apiRouter;
